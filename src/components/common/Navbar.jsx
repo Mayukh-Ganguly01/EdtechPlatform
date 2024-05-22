dae1c5d5f01ad42c11077d2cb53b267a6453fe10
Mayukh-Ganguly01 <mayukhganguly.cse2021@nsec.ac.in>
2024-05-22 22:37:06 +0530

    few changes

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import logo from '../../assets/Logo/Logo-Full-Light.png'
-import { Link } from 'react-router-dom'
+import { Link, matchPath } from 'react-router-dom'
 import { NavbarLinks } from '../../data/navbar-links'
+import { useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 
 const Navbar = () => {
+
+    const{token} = useSelector((state) => state.auth)
+    const{user} = useSelector((state)=> state.profile)
+    const{totalItems} = useSelector((state) => state.cart)
+    const location = useLocation()
+    const matchRoute = (route) => {
+        return matchPath({path:route}, location.pathname)
+    }
+
   return (
     <div className='flex h-14 items-center justify-center border-b-[1px] border-white'>
         <div className='w-8/12 flex max-w-maxContent items-center justify-between'>
         {/* logo */}
             <Link to="/">
-                <img src={logo} width={160} height={32} loading='lazy'/>
+                <img src={logo} width={160} alt="img" height={32} loading='lazy'/>
             </Link>
 
         {/* navlinks */}
@@ -22,7 +33,7 @@ const Navbar = () => {
                             {
                                 link.title === "Catalog" ? (<div></div>) : (
                                     <Link to={link?.path}>
-                                        <p className='text-yellow-25'>{link.title}</p>
+                                    <p className={`${matchRoute(link?.path) ? "text-yellow-25":"text-richblack-25"}`}>{link.title}</p>
                                     </Link>
                                 )
                             }
@@ -31,6 +42,13 @@ const Navbar = () => {
                 }
             </ul>
         </nav>
+
+        {/* login/signup/dashboard */}
+        <div className='flex gap-x-4 items-center'>
+
+        </div>
+            
+
         </div>
     </div>
   )
